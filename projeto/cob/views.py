41a6aeaf985d9c443711460d819dd2d26b813762
Gabriel Vidal <gbastosvidal@gmail.com>
2022-07-21 10:20:12 -0300

    adicionando css

--- projeto/cob/views.py
+++ projeto/cob/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'cob/home.html', context={'name': 'Gabriel Vidal,'})
+    return render(request, 'cob/pages/home.html', context={'name': 'Gabriel Vidal,'})
 
