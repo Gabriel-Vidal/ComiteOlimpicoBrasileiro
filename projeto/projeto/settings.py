4b5640ad5301ba15c39c52d49fefa086ed433617
Gabriel Vidal <gbastosvidal@gmail.com>
2022-07-22 09:55:49 -0300

    css movido para arq estatico, footer adicionado

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cob'
+    'cob',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
