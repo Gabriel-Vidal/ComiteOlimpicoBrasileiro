de307bdd8b91aecf82a8deaa73f24435fc2a24e4
Gabriel Vidal <gbastosvidal@gmail.com>
2022-08-04 11:40:57 -0300

    models criados

--- projeto/cob/models.py
+++ projeto/cob/models.py
@@ -1,3 +1,17 @@
+from platform import mac_ver
+
 from django.db import models
 
-# Create your models here.
+
+class Competicao(models.Model):
+    nome = models.CharField(max_length=65)
+    data = models.DateField()
+    descricao = models.CharField(max_length=100)
+
+
+class Atletas(models.Model):
+    nome = models.CharField(max_length=65)
+    idade = models.IntegerField()
+    peso = models.FloatField()
+    altura = models.FloatField()
+    competicao = models.ForeignKey(Competicao, on_delete=models.SET_NULL, null=True)
